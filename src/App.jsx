d3cc30a9738dea6c3d97e5b6cc25aea58dcb12e2
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-06-09 20:10:07 +0700

    feat: create search box and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  const searchBook = async (text) => {
+    try {
+      const data = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${text}`
+      );
+      setSearchResult(data.data.items);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (searchText) {
+      searchBook(searchText);
+    }
+  }, [searchText]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" onChange={(e) => setSearchText(e.target.value)} />
+      {searchResult.map((item, index) => (
+        <div key={index}>
+          <li>{item["volumeInfo"].title}</li>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
